0e24c4262b23d29ec5911e1780610d47abddb385
gab1144 <gabry.rinciari04@gmail.com>
2023-01-18 18:00:19 +0100

    [add] button to index

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,6 +7,7 @@
             <img class="detail-img" src="{{$project->cover_image}}" alt="{{$project->name}}">
             <div>Client name: <strong>{{$project->client_name}}</strong></div>
             <div>Summary: <strong>{{$project->summary}}</strong></div>
+            <a class="btn btn-primary text-white" href="{{route('admin.projects.index', $project)}}" title="edit">Back to list</i></a>
             <a class="btn btn-warning text-white" href="{{route('admin.projects.edit', $project)}}" title="edit"><i class="fa-solid fa-pencil"></i></a>
             @include('admin.partials.form-delete')
         </div>
