355b0f21599d984fbe7d5f2efc20f73cf8407324
gab1144 <gabry.rinciari04@gmail.com>
2023-01-20 18:00:12 +0100

    [change] cards to table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,19 +8,30 @@
             <h1>Projects</h1> <a href="{{route('admin.projects.create')}}" class="add-button btn btn-primary"><i class="fa-solid fa-plus"></i></a>
         </div>
 
-        @foreach ($projects as $project)
-        <div class="col-md-6 col-lg-4 my-2">
-            <div class="card" style="width: 18rem;">
-                <img src="{{$project->cover_image}}" alt="{{$project->name}}">
-                <div class="card-body">
-                  <h5 class="card-title">{{$project->name}}</h5>
-                  <a href="{{route('admin.projects.show', $project)}}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
-                  <a class="btn btn-warning " href="{{route('admin.projects.edit', $project)}}" title="edit"><i class="fa-solid fa-pencil text-white"></i></a>
-                  @include('admin.partials.form-delete')
-                </div>
-            </div>
-        </div>
-        @endforeach
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome progetto</th>
+                <th scope="col">Nome cliente</th>
+                <th scope="col">Azioni</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <th>{{$project->id}}</th>
+                        <td>{{$project->name}}</td>
+                        <td>{{$project->client_name}}</td>
+                        <td><a href="{{route('admin.projects.show', $project)}}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
+                        <a class="btn btn-warning " href="{{route('admin.projects.edit', $project)}}" title="edit"><i class="fa-solid fa-pencil text-white"></i></a>
+                        @include('admin.partials.form-delete')
+                        </td>
+                    <tr>
+                @endforeach
+            </tbody>
+          </table>
+
 
         {{$projects->links()}}
     </div>
