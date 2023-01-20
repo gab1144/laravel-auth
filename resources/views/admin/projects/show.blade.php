b24a87732e4537b6edc216c4e15b8f3cce683d9b
gab1144 <gabry.rinciari04@gmail.com>
2023-01-20 18:06:26 +0100

    [change] dimensione immagine e scritte

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,9 +7,9 @@
             @if($project->cover_image)
                 <img class="detail-img" src="{{asset('storage/'.$project->cover_image)}}" alt="{{$project->cover_image_original_name}}">
             @endif
-            <div>Client name: <strong>{{$project->client_name}}</strong></div>
-            <div>Summary: <strong>{{$project->summary}}</strong></div>
-            <a class="btn btn-primary text-white" href="{{route('admin.projects.index', $project)}}" title="edit">Back to list</i></a>
+            <div>Nome cliente: <strong>{{$project->client_name}}</strong></div>
+            <div>Sommario: {!!$project->summary!!}</div>
+            <a class="btn btn-primary text-white" href="{{route('admin.projects.index', $project)}}" title="edit">Torna alla lista</i></a>
             <a class="btn btn-warning text-white" href="{{route('admin.projects.edit', $project)}}" title="edit"><i class="fa-solid fa-pencil"></i></a>
             @include('admin.partials.form-delete')
         </div>
